ad6d82e4a52fb3025184f8ac64bfc8f99037ccd2
kartik20 <kartikkartik29957@gmail.com>
2025-04-21 14:05:40 +0530

    ka

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-      origin :['http://localhost:3000', 'https://cs-315-frontend.vercel.appg'],
+      origin :['http://localhost:3000', 'https://cs-315-frontend.vercel.app'],
       methods:["GET", "POST", "PATCH", "DELETE"],
       credentials: true
   }
